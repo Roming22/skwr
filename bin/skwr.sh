fb030f0aba1aa60b9704369e743176e16f7e8273
Roming22 <roming22@gmail.com>
2018-06-05 04:47:30 +0000

    Beautify skwr.sh and prepare for auto-completion generation

--- bin/skwr.sh
+++ bin/skwr.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 SCRIPT_DIR=`cd $(dirname $(readlink -f $0)); pwd`
-COMMAND=$1
-shift
 
-MODULE_DIR="$PWD"
-while [[ ! -e "$MODULE_DIR/docker" && ! -e "$MODULE_DIR/etc" ]]; do
-	MODULE_DIR=`cd $MODULE_DIR/..; pwd`
-	if [[ "$MODULE_DIR" = "/" ]]; then
-		unset MODULE_DIR
-		break
-	fi
-done
+usage(){
+echo "
+Commands:
+`for C in $(find $SCRIPT_DIR -name run.sh -exec dirname {} \;); do echo "  $(basename $C)"; done`
 
-while [[ "$#" -gt 0 ]]; do
-	case $1 in
-		-v) set -x; VERBOSE="-v" ;;
-		*)	[[ -d "$SCRIPT_DIR/../modules/$1" ]] && MODULE_DIR="$SCRIPT_DIR/../modules/$1"
-			[[ -d "$PWD/$1" ]] && MODULE_DIR=`cd $1; pwd`
-			
-	esac
-	shift
-done
+Flags:
+  -h,--help	      show this message
+  -v,--verbose    increase verbose level
+"
+}
 
-if [[ -z "$MODULE_DIR" ]]; then
-	echo "[ERROR] No module specified"
-	echo "Known modules: `find ../modules -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | sort | paste -sd "," - | sed 's:,:, :g'`"
-	exit 1
-fi
+init(){
+  SCRIPT_DIR=`cd $(dirname $(readlink -f $0)); pwd`
+  MODULE_DIR="$PWD"
+  while [[ ! -e "$MODULE_DIR/docker" && ! -e "$MODULE_DIR/etc" ]]; do
+    MODULE_DIR=`cd $MODULE_DIR/..; pwd`
+    if [[ "$MODULE_DIR" = "/" ]]; then
+      unset MODULE_DIR
+      break
+    fi
+  done
+}
 
-$SCRIPT_DIR/$COMMAND/run.sh $MODULE_DIR $VERBOSE
+parse_args(){
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -h|--help) usage; exit 0 ;;
+      -v|--verbose) set -x; VERBOSE="-v" ;;
+      *) COMMAND=$1; shift; break ;;
+    esac
+    shift
+  done
+}
+
+init
+parse_args $*
+$SCRIPT_DIR/$COMMAND/run.sh $* $VERBOSE
 
