f689e282e555e9482000e4738231d7bdf81a8020
Roming22 <roming22@gmail.com>
2018-09-09 16:31:41 -0400

    bugfix: [host] Docker installation fails. Network interfaces are not renamed before reboot

--- bin/host/archarm/run.sh
+++ bin/host/archarm/run.sh
@@ -11,15 +11,15 @@ set -e
 install_packages(){
 	pacman-key --init
 	pacman-key --populate archlinuxarm
+	pacman -Sy
 	for PACKAGE in docker git rsync sudo vim; do
 		echo "Installing $PACKAGE..."
-		pacman -Syu --noconfirm $PACKAGE
+		pacman -S --noconfirm $PACKAGE
 		case $PACKAGE in
 			docker)
-				cat /etc/group | egrep -q "^docker:" || groupadd docker
-				usermod -aG docker pi
-				systemctl enable docker
 				systemctl start docker
+				systemctl enable docker
+				egrep -q "^docker:" /etc/group || groupadd docker
 				;;
 			sudo)
 				__rsync /etc/sudoers.d
@@ -29,6 +29,7 @@ install_packages(){
 				;;
 		esac
 	done
+	pacman -Syu --noconfirm
 }
 
 
@@ -38,6 +39,7 @@ setup_users(){
 	if [[ ! -e "/home/pi" ]]; then
 		echo "Creating user pi ..."
 		useradd -m pi
+		usermod -aG docker pi
 		echo "pi:pi" | chpasswd
 		__rsync /home/pi
 		chown -R pi:pi /home/pi
@@ -56,27 +58,29 @@ setup_users(){
 
 # Deploy network configuration
 setup_network(){
+	# Change the hostname
 	hostnamectl set-hostname rpi
 
-	# Setup interfaces
-	rm -f /etc/systemd/network/*.network /etc/systemd/network/*.link
-	__rsync /etc/systemd/network
-	__rsync /etc/netctl
+	# Initialize some variables
 	WLAN_MAC=`cat /sys/class/net/wlan0/address`
 	SOC_MAC=`cat /sys/class/net/*/address | grep $(echo $WLAN_MAC | cut -d: -f1-3) | grep -v $WLAN_MAC`
 	SOC_NAME=`grep $SOC_MAC /sys/class/net/*/address | cut -d: -f1 | cut -d/ -f5`
 	USB_MAC=`cat /sys/class/net/$(ls /sys/class/net/ | egrep -v "^lo$|^$SOC_NAME$|^wlan0$|^docker0$|^br-|^veth")/address`
+	USB_NAME=`grep $USB_MAC /sys/class/net/*/address | cut -d: -f1 | cut -d/ -f5`
+
+	# Rename interfaces
+	ifconfig $SOC_NAME down
+	ip link set $SOC_NAME name wan0
+	ifconfig wan0 up
+	ifconfig $USB_NAME down
+	ip link set $USB_NAME name lan0
+	ifconfig lan0 up
+
+	# Persist change on reboot
+	rm -f /etc/systemd/network/*.network /etc/systemd/network/*.link
+	__rsync /etc/systemd/network
 	sed -i -e "s|MACAddress=.*|MACAddress=$SOC_MAC|" /etc/systemd/network/10-ethsoc.link
 	sed -i -e "s|MACAddress=.*|MACAddress=$USB_MAC|" /etc/systemd/network/10-ethusb0.link
-
-	# Restart network if necessary
-	NEW_IP=`egrep "^Address" /etc/netctl/lan0-profile | cut -d"'" -f2 | cut -d/ -f1`
-	if [[ `ifconfig eth0 | grep 'inet ' | awk '{print $2}'` != $NEW_IP ]]; then
-		echo "Network interface is being restarted. Use $NEW_IP to connect to the server."
-	fi
-	for interface in `egrep '^Name=' /etc/systemd/network/*.network | cut -d= -f2`; do
-		netctl enable ${interface}-profile
-	done
 }
 
 __rsync(){
